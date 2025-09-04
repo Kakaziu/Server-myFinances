94829a2137808c703fc4e86aabd576dad80b0c36
Kauã <kaua.borba03@gmail.com>
2025-09-03 22:34:23 -0300

    fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+app.use(cors())
 require('./src/config/dbConfig')
   .then(res =>{
     console.log('Banco carregado.') 
